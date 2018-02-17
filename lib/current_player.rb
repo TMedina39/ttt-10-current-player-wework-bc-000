de37ba132fe11344aca02c80e7288ccc5285b3af
Tomas Medina <tomas.medina@wework.com>
2018-02-17 21:53:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,5 @@
 board [" "," "," "," "," "," "," "," "," "]
+
   def turn_count (board)
     turn = 1
     board.each do |space|
@@ -6,3 +7,14 @@ board [" "," "," "," "," "," "," "," "," "]
         turn += 1
     end
   end
+
+  def current_player (turn)
+    if turn % 2 == 0
+      "O"
+    else
+      "X"
+    end
+  end
+
+turn = turn_count(board)
+puts "It is currently turn # #{turn}."
